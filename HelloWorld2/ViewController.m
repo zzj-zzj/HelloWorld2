72a0268e32ba190f58e7759a981062a9d8f4d45f
许小六 <xuxiaoliu2847@gmail.com>
2018-09-03 16:52:57 +0800

    添加上传
    上传本地文件

---
+++ HelloWorld2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloWorld2
+//
+//  Created by 许小六 on 2018/9/3.
+//  Copyright © 2018年 许小六. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
